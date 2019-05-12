357b2d711c9731372ed9b2ff6cb892f3c9224579
albatroscom <albatroscom@naver.com>
2019-05-12 17:32:36 +0900

    isSelf and isFollowing Fileds part Two

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,25 +5,30 @@ export default {
         fullName: parent => {
             return `${parent.firstName} ${parent.lastName}`;
         },
-        amIFollowing: async(parent, _, {request}) => {
+        isFollowing: async(parent, _, {request}) => {
             const { user } = request;
             const { id: parentId } = parent;
             try {
-                const exist = await prisma.$exists.user({
-                    AND: [{ id: parentId}, { followers_some: [user.id] }]
+                return await prisma.$exists.user({
+                    AND: [
+                        { 
+                            id: parentId
+                        }, 
+                        { 
+                            // followers_some: [user.id]
+                            followers_some: {
+                                id: user.id
+                            }
+                        }
+                    ]
                 });
-                console.log('exist : ', exist);
-                if(exist){
-                    return true;
-                } else {
-                    return false;
-                }
+                // return exist;
             } catch (error) {
                 console.log(error);
                 return false;
             }
         },
-        itsMe: (parent, _, {request}) => {
+        isSelf: (parent, _, {request}) => {
             const { user } = request;
             const { id: parentId } = parent;
             return user.id === parentId; // 요청하는 사람(request)이 같으면 내 프로필을 요청하는 것
