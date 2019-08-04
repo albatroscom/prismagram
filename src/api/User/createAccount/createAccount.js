0be4dca3f8ff0fbb68ada3fedbee3f9681481b77
albatroscom <albatroscom@naver.com>
2019-08-04 19:42:50 +0900

    [Authorization] confirm Secret + Login Mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,18 @@ export default {
     Mutation: {
         createAccount: async(_, args, { request }) => {
             const { userName, email, firstName="", lastName="", bio="" } = args;
+            const exists = await prisma.$exists.user({ 
+                OR: [
+                    {
+                        userName
+                    },
+                    { email }
+                ]
+            });
+
+            if(exists){
+                throw Error('This userName OR email is already taken');
+            }
             await prisma.createUser({
                 userName,
                 email,
